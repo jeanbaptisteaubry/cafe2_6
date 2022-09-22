755c0b5e3c222ad34816697283e159cc6598d8ad
superjb1664 <jbaubry25@gmail.Com>
2022-09-22 13:24:47 +0200

    C1

---
+++ demandeInscriptionEntreprise.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            Une page
+        </title>
+    </head>
+    <body>
+        <form action="reponseInscriptionEntreprise.php" method="GET">
+            Dénomination sociale <input type="text" name="denominationSociale"><br>
+            Raison sociale <input type="text" name="raisonSociale"><br>
+            Adresse <input type="text" name="Adresse"><br>
+            CP <input type="text" name="CP"><br>
+            Ville <input type="text" name="Ville"><br>
+            Mail contact <input type="text" name="mailContact"><br>
+            <input type="submit" name="button_envoyer" value ="Envoyer">
+        </form>
+    </body>
+</html>
